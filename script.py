76b10937b7c88c897a0397b8e1b66673ec719ac3
Zayd Alameddine <zayd.alameddine@gmail.com>
2020-05-09 19:09:50 -0400

    Update

---
+++ script.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 15:43:43 2020
+
+@author: Zayd Alameddine
+"""
+
+from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+netflix_stocks = 
